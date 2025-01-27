ff1afd8ceb4169c035ebe816cb6c427e8498450e
Tanbir Pradhan <pradhantanbir@gmail.com>
2025-01-27 10:05:17 +0530

    task 2

--- main.go
+++ main.go
@@ -21,30 +21,28 @@ type QueryResponse struct {
 	Error   string
 }
 
-var db *sql.DB
+var (
+	db              *sql.DB
+	authError       string
+	createUserError string
+	isRoot          bool = false
+)
 
 func main() {
-	var err error
-	db, err = sql.Open("mysql", "root:Tanbir@12345@tcp(localhost:3306)/testdb")
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatalf("Database connection failed: %v", err)
-	}
-
-	http.HandleFunc("/", executeQuery)
-
+	http.HandleFunc("/", login)
+	http.HandleFunc("/executeQuery", executeQueryHandler())
+	http.HandleFunc("/createUser", createUser)
 	log.Println("Server is running on port 8088...")
 	log.Fatal(http.ListenAndServe(":8088", nil))
 }
 
-func executeQuery(w http.ResponseWriter, r *http.Request) {
-	data := QueryRequest{}
-	response := QueryResponse{}
+// Login function
+func login(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/login.html")
+	if err != nil {
+		http.Error(w, "Unable to load template", http.StatusInternalServerError)
+		return
+	}
 
 	if r.Method == http.MethodPost {
 		err := r.ParseForm()
@@ -53,67 +51,168 @@ func executeQuery(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		data.Submitted = true
-		data.Query = r.FormValue("queries")
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		// Database Connection
+		authString := fmt.Sprintf("%s:%s@tcp(localhost:3306)/information_schema", username, password)
+		db, err = sql.Open("mysql", authString)
+		if err != nil {
+			authError = "Failed to connect to database"
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+
+		err = db.Ping()
+		if err != nil {
+			authError = "Invalid username or password"
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+
+		// Giving access to create user who has Create_user_priv
+		row := db.QueryRow("SELECT Create_user_priv FROM mysql.user WHERE User = ?", username)
+		var createUserPriv string
+		if err := row.Scan(&createUserPriv); err != nil {
+			authError = "Failed to check user privileges"
+		} else {
+			isRoot = (createUserPriv == "Y")
+		}
+
+		authError = ""
+		http.Redirect(w, r, "/executeQuery", http.StatusSeeOther)
+		return
 	}
 
-	tmpl, err := template.ParseFiles("templates/index.html")
+	tmpl.Execute(w, struct {
+		Error string
+	}{
+		Error: authError,
+	})
+}
+
+// Query handler
+func executeQueryHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if db == nil {
+			http.Error(w, "Database connection not established", http.StatusInternalServerError)
+			return
+		}
+
+		data := QueryRequest{}
+		response := QueryResponse{}
+
+		if r.Method == http.MethodPost {
+			err := r.ParseForm()
+			if err != nil {
+				http.Error(w, "Error parsing form", http.StatusBadRequest)
+				return
+			}
+
+			data.Submitted = true
+			data.Query = r.FormValue("queries")
+		}
+
+		tmpl, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			http.Error(w, "Unable to load template", http.StatusInternalServerError)
+			return
+		}
+
+		if data.Query != "" {
+			rows, err := db.Query(data.Query)
+			if err != nil {
+				response.Error = fmt.Sprintf("Query error: %v", err)
+			} else {
+				defer rows.Close()
+
+				response.Columns, err = rows.Columns()
+				if err != nil {
+					response.Error = fmt.Sprintf("Error retrieving columns: %v", err)
+				} else {
+					for rows.Next() {
+						record := make([]interface{}, len(response.Columns))
+						recordPtrs := make([]interface{}, len(response.Columns))
+						for i := range record {
+							recordPtrs[i] = &record[i]
+						}
+
+						if err := rows.Scan(recordPtrs...); err != nil {
+							response.Error = fmt.Sprintf("Error scanning row: %v", err)
+							return
+						}
+
+						for i, rawValue := range record {
+							if b, ok := rawValue.([]byte); ok {
+								record[i] = string(b)
+							}
+						}
+
+						response.Results = append(response.Results, record)
+					}
+				}
+			}
+		}
+
+		err = tmpl.Execute(w, struct {
+			Query string
+			QueryResponse
+			Submitted bool
+			Root      bool
+		}{
+			Query:         data.Query,
+			QueryResponse: response,
+			Submitted:     data.Submitted,
+			Root:          isRoot,
+		})
+		if err != nil {
+			http.Error(w, "Error rendering template", http.StatusInternalServerError)
+		}
+	}
+}
+
+// __________________________________   Create user  function   _________________________________________
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/register.html")
 	if err != nil {
 		http.Error(w, "Unable to load template", http.StatusInternalServerError)
 		return
 	}
 
-	if data.Query != "" {
-
-		// If the query have prefix select but no limit
-		// if !strings.Contains(strings.ToLower(data.Query), "limit") && strings.HasPrefix(strings.ToLower(data.Query), "select") {
-		// 	query = query[:len(query)-1] + " LIMIT 1 ;"
-		// }
-
-		rows, err := db.Query(data.Query)
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
 		if err != nil {
-			response.Error = fmt.Sprintf("Query error: %v", err)
-		} else {
-			defer rows.Close()
-
-			response.Columns, err = rows.Columns()
-			if err != nil {
-				response.Error = fmt.Sprintf("Error retrieving columns: %v", err)
-			} else {
-				for rows.Next() {
-					record := make([]interface{}, len(response.Columns))
-					recordPtrs := make([]interface{}, len(response.Columns))
-					for i := range record {
-						recordPtrs[i] = &record[i]
-					}
-
-					if err := rows.Scan(recordPtrs...); err != nil {
-						response.Error = fmt.Sprintf("Error scanning row: %v", err)
-						return
-					}
-
-					for i, rawValue := range record {
-						if b, ok := rawValue.([]byte); ok {
-							record[i] = string(b)
-						}
-					}
-
-					response.Results = append(response.Results, record)
-				}
-			}
+			http.Error(w, "Error parsing form", http.StatusBadRequest)
+			return
 		}
+
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		query := fmt.Sprintf("CREATE USER '%s'@'localhost' IDENTIFIED BY '%s';", username, password)
+
+		// Execute the query
+		_, err = db.Exec(query)
+		if err != nil {
+			createUserError = "Failed to create user"
+		} else {
+			createUserError = "User created successfully"
+		}
+
+		if err != nil {
+			createUserError = "Failed to connect to database"
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+
+		http.Redirect(w, r, "/executeQuery", http.StatusSeeOther)
+		return
 	}
 
-	err = tmpl.Execute(w, struct {
-		Query string
-		QueryResponse
-		Submitted bool
+	tmpl.Execute(w, struct {
+		Error string
 	}{
-		Query:         data.Query,
-		QueryResponse: response,
-		Submitted:     data.Submitted,
+		Error: createUserError,
 	})
-	if err != nil {
-		http.Error(w, "Error rendering template", http.StatusInternalServerError)
-	}
 }
